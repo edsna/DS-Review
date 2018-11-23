ebf25ab99677858b278f3304f8c10e83eb8a2a22
Edson Zandamela <edsonaguiar17@gmail.com>
2018-11-23 18:39:01 -0500

    Methods with parameters

--- Methods.java
+++ Methods.java
@@ -4,11 +4,19 @@ import java.util.*;
  * @author Edson Zandamela
  * This prigram performs basic method/function calls 
 */
+	
 public class Methods{
-
-	//Main
-	public static void main(String[]args){
-		System.out.println("Simple line printout from Main");
+	//Prints simple statement
+	public static void firstMethod(){
+		System.out.println("Printput from a Method");
 	}
 	
+	//Main
+	public static void main(String[]args){
+		System.out.println("Printout from Main");
+
+		//Calls Method
+		firstMethod();
+	}
+
 }
