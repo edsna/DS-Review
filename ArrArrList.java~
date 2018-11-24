0a11cecc55e36412240c676bc64f0ad796b3cdb9
Edson Zandamela <edsonaguiar17@gmail.com>
2018-11-23 19:48:58 -0500

    Basic on Arrays

---
+++ ArrArrList.java~
@@ -0,0 +1,7 @@
+public class ArrArrList{
+
+	//Main
+	public static void main(String[] args){
+		
+	}
+}
---
+++ Arrays.java
@@ -0,0 +1,19 @@
+public class Arrays{
+	//Cann only store values of the same datatype
+
+	
+	//Main
+	public static void main(String[] args){
+		//creates a new integer array
+		int myArray[] = new int [10];
+		myArray[0] = 10;	//Adds array element in specific loc
+		myArray[1] = 10;
+		myArray[2] = 10;
+		myArray[3] = 10;
+		myArray[4] = 10;
+		myArray[5] = 10;
+		myArray[6] = 10;
+		myArray[7] = 10;
+		System.out.println(myArray[9]);	//Prints 1 array element
+	}
+}
