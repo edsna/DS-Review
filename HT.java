0dd637e024bee489a1456cd9d9e358134cf83990
Edson Zandamela <edsonaguiar17@gmail.com>
2018-11-24 22:50:59 -0500

    docs: Add HT.java

---
+++ HT.java
@@ -0,0 +1,16 @@
+/*@Author: Edson Zandamela
+* Hash Tables
+*/
+//DS used to store info with a Key(Name) and Value(Phone#)
+//Hask Function>> Looks at a Key >> evaluate it & spit some index# that's going be stored somewhere in the array
+//Eg: Hash(edson) = index 3 >>best index data can go in
+//If collision is found, it creates a LinkedList on that same index position
+//Taking a key value it brings the index position in the array.
+//Fast in insertion and sorting to an array iwith limited size
+
+public class HT{
+	
+	public static void main(String[]args){
+
+	}
+}
