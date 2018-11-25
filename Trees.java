7b490cc25c0bf753fdfd0ef020e54fc687b5f6ae
Edson Zandamela <edsonaguiar17@gmail.com>
2018-11-24 21:37:45 -0500

    docs:Add Node.java class with insert(), contains(), and orinInorder() methods

---
+++ Trees.java
@@ -0,0 +1,27 @@
+/*
+* @author Edson Zandamela
+*/
+//BT >> each node has no more than 2 childs
+//BST >> Left node < all right nodes
+//Finding nodes is fast(compare x with rootNode then take right path to find it)
+//Inserts >> cmp with root (if > go right; if < go left...till null-add)
+//Traversing: inorder(LRoRi)"allows print in order" preorder(RLR), postorder(LRiRo)
+
+//IMPLEMENTATION of BST
+public class Trees{
+//mIGHT NEED TO CHANGE CLASS NAME
+	class Node{
+		Node left, right;
+		int data;
+		//Constructor
+		public Node(int data){
+			
+		}
+	}
+
+
+	
+	public static main(String[]args){
+
+	}
+}
