5e0551f4c639c6f7a965daa4992024aa5282a0ae
Edson Zandamela <edsonaguiar17@gmail.com>
2018-11-25 23:27:18 -0500

    Docs:m Add hashCONtains.java

---
+++ hashCOntains.java
@@ -0,0 +1,27 @@
+import java.util.*; 	
+/*
+ * @author: Edson Zandamale
+ * This program implements the contains() method from a Hash table >> Checks weather or not avalue exists in the Hashtable
+*/
+
+public class hashCOntains{ 
+	public static void main(String[] arg) 
+	{ 
+		// creates a Hashtable
+		Hashtable myHasT = new Hashtable(); 
+			
+		// enter name/myHashT pair 
+		myHasT.put("Edson", new Integer(100)); 
+		myHasT.put("Francisco", new Double(150)); 
+		myHasT.put("de", new Integer(80.88)); 
+		myHasT.put("Aguiar", new Double(120.0)); 
+		myHasT.put("Zandamela", new Integer(1000)); 
+			
+		
+		// check whether a value exists or not 
+		if (marks.contains(1000)) 
+			System.out.println("Value found in table"); 
+		
+	} 
+}
+
